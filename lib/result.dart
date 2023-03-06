e0f884a30e0a4904a14612de59aec0263840dca4
VaishnavPCP <120624785+VaishnavPCP@users.noreply.github.com>
2023-03-06 11:58:08 +0530

    Commit1

--- lib/result.dart
+++ lib/result.dart
@@ -1,124 +1,56 @@
+import 'package:bmiadvanced/utils.dart';
 import 'package:flutter/material.dart';
 import 'package:bmiadvanced/designpage.dart';
+
 class Result extends StatelessWidget {
-  Result({required this.bmiresult, required this.resulttext, required this.inter});
-
-final String bmiresult;
-final String resulttext;
-
-final String inter;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.blueGrey,
-      appBar: AppBar(title: Text("BMI CALCULATOR"), centerTitle: true,
-        backgroundColor: Colors.blueGrey,),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Text("Your Result", style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 20,
-              color: Colors.white)),
-          Container(
-            height: 400,
-            width: double.maxFinite,
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.black12),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Text(resulttext, style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 40,
-                    color: Colors.black)),
-                Text(bmiresult, style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 70,
-                    color: Colors.white)),
-                Text(inter, style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                    color: Colors.white),),
-              ],
-            ),
-          ),
-          TextButton(
-              style: ElevatedButton.styleFrom(backgroundColor: Colors.pink),
-              onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context)=>Design()));
-              },
-              child: Text(
-                "Calculate Again",
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                    color: Colors.white),
-              )),
-
-        ],
-      ),
-    );
-  }
-}
-
-/*
-
-
-class Result extends StatefulWidget {
-  Result({required this.bmiresult, required this.resulttext, required this.inter});
+  Result(
+      {required this.bmiresult, required this.resulttext, required this.inter});
 
   final String bmiresult;
   final String resulttext;
+
   final String inter;
-  @override
-  State<Result> createState() => _ResultState();
-}
-class _ResultState extends State<Result> {
 
   @override
   Widget build(BuildContext context) {
+    var textStyle = const TextStyle(
+        fontWeight: FontWeight.bold, fontSize: 40, color: Colors.black);
     return Scaffold(
       backgroundColor: Colors.blueGrey,
-      appBar: AppBar(title: Text("BMI CALCULATOR"), centerTitle: true,
-        backgroundColor: Colors.blueGrey,leading: Icon(Icons.arrow_back),),
+      appBar: AppBar(
+          title: Text("BMI CALCULATOR"),
+          centerTitle: true,
+          backgroundColor: Colors.blueGrey,
+          leading: IconButton(
+            onPressed: () {
+              Navigator.push(
+                  context, MaterialPageRoute(builder: (context) => Design()));
+            },
+            icon: Icon(Icons.arrow_back),
+          )),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          Text("Your Result", style: TextStyle(
-          fontWeight: FontWeight.bold,
-          fontSize: 20,
-          color: Colors.white)),
+          Text("Your Result", style: textStyle),
           Container(
             height: 400,
             width: double.maxFinite,
             decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.black12),
+                borderRadius: BorderRadius.circular(10), color: Colors.black12),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                Text(r, style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                    color: Colors.white)),
-                Text("00000", style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                    color: Colors.white)),
-                Text("You Are Normal", style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                    color: Colors.white),),
+                Text(resulttext, style: txt(40, Colors.white, FontWeight.bold)),
+                Text(bmiresult, style: txt(70, Colors.white, FontWeight.bold)),
+                Text(inter, style: txt(20, Colors.white, FontWeight.bold)),
               ],
             ),
           ),
           TextButton(
               style: ElevatedButton.styleFrom(backgroundColor: Colors.pink),
               onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context)=>Design()));
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (context) => Design()));
               },
               child: Text(
                 "Calculate Again",
@@ -127,10 +59,8 @@ class _ResultState extends State<Result> {
                     fontSize: 20,
                     color: Colors.white),
               )),
-
         ],
       ),
     );
   }
 }
-*/
---
+++ lib/utils.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+TextStyle txt(double size,[Color? color,FontWeight? fntwght]){
+  return TextStyle(
+      color: color,
+      fontSize: size,
+      fontWeight: fntwght
+  );
+}
